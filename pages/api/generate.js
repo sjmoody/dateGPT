fc0314459584d7cef09497bee711fc155d04b06b
Steven Moody <smoody07@gmail.com>
2023-03-17 15:34:52 -0300

    try alt gpt4 model

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -46,11 +46,12 @@ const generateAction = async (req, res) => {
 
 
     const baseCompletion = await openai.createCompletion({
-      model: 'gpt-4',
+      model: "gpt-4-0314",
       prompt: `${basePromptPrefix}${basePromptInputs}`,
       temperature: 0.70,
       max_tokens: 500,
     });
+
 
     const basePromptOutput = baseCompletion.data.choices.pop();
     res.status(200).json({ output: basePromptOutput });
